8eae7e762af3abc5b5a9fe045607d7e6c4a08d4d
xforeverjlx <johannchrislee@gmail.com>
2021-11-27 18:54:37 -0500

    fixed plotStuff

--- tutorials/utils/plotStuff.py
+++ tutorials/utils/plotStuff.py
@@ -31,7 +31,7 @@ def play_random_episode(env, plot_every=100, do_dense_logging=False):
     if ((t+1) % plot_every) != 0:
         do_plot(env, ax, fig)        
 
-        
+
 # Play another episode. This time, tell the environment to do dense logging
 play_random_episode(env, plot_every=100, do_dense_logging=True)
 
@@ -41,4 +41,6 @@ fig = plotting.vis_world_range(dense_log, t0=0, tN=200, N=5)
 fig.savefig('vis_world_range.png')
 
 # # Use the "breakdown" tool to visualize the world state, agent-wise quantities, movement, and trading events
-# plotting.breakdown(dense_log);
+# plotting.breakdown(dense_log); 
+
+#CHANGES CHANGES CHANGES
