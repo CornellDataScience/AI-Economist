2bb419c1adaa49886c704b104cd7296cd3077609
Evelyn <ew423@cornell.edu>
2021-11-29 00:35:02 -0500

    bugfix 6

--- tutorials/rllib/training_script.py
+++ tutorials/rllib/training_script.py
@@ -330,9 +330,9 @@ if __name__ == "__main__":
             episodes_per_replica = 0
 
         if episodes_per_replica == 1 or (episodes_per_replica % dense_log_frequency) == 0:
-            actor_reward_stats = actor_reward_stats + curr_iter + "," + str(result["policy_reward_max"]["a"]) + "," + str(result["policy_reward_mean"]["a"]) + "," + str(result["policy_reward_min"]["a"]) + "\n"
-            policymaker_reward_stats = policymaker_reward_stats + curr_iter + "," + str(result["policy_reward_max"]["p"]) + "," + str(result["policy_reward_mean"]["p"]) + "," + str(result["policy_reward_min"]["p"]) + "\n"
-            tax_policy_per_period = tax_policy_per_period + curr_iter + "," + trainer.previous_episode_dense_log["PeriodicTax"]["schedule"] + "," + trainer.previous_episode_dense_log["PeriodicTax"]["cutoffs"] + "\n"
+            actor_reward_stats = actor_reward_stats + str(curr_iter) + "," + str(result["policy_reward_max"]["a"]) + "," + str(result["policy_reward_mean"]["a"]) + "," + str(result["policy_reward_min"]["a"]) + "\n"
+            policymaker_reward_stats = policymaker_reward_stats + str(curr_iter) + "," + str(result["policy_reward_max"]["p"]) + "," + str(result["policy_reward_mean"]["p"]) + "," + str(result["policy_reward_min"]["p"]) + "\n"
+            tax_policy_per_period = tax_policy_per_period + str(curr_iter) + "," + trainer.previous_episode_dense_log["PeriodicTax"]["schedule"] + "," + trainer.previous_episode_dense_log["PeriodicTax"]["cutoffs"] + "\n"
 
         # === Saez logic ===
         maybe_sync_saez_buffer(trainer, result, run_config)
