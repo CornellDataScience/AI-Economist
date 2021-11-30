2287ea2f7df306a491b3b7cdab23ce939e1caab0
xforeverjlx <johannchrislee@gmail.com>
2021-11-30 17:36:14 -0500

    breakdown chart

--- tutorials/utils/plotStuff.py
+++ tutorials/utils/plotStuff.py
@@ -8,8 +8,10 @@ import os
 base = "../rllib/phase1/dense_logs/"
 log_path = os.path.join(base, "logs_0000000024600000/env000.lz4")
 dense_log = load_episode_log(log_path)
+fig2 = plotting.breakdown(dense_log)
 fig = plotting.vis_world_range(dense_log, t0=0, tN=200, N=5)
 fig.savefig('vis_world_range.png') 
+fig2.savefig('breakdown.png')
 
 
 # from ai_economist import foundation
