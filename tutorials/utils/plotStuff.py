503f559d87190aaf06204b0675ebff45657295e8
Evelyn <ew423@cornell.edu>
2021-12-03 16:45:14 -0500

    k

--- tutorials/utils/plotStuff.py
+++ tutorials/utils/plotStuff.py
@@ -5,9 +5,9 @@ from ai_economist.foundation.utils import load_episode_log
 import plotting
 import os
 
-base = "../rllib/phase1/dense_logs/"
-file_loc = "../rllib/phase1/ckpts/"
-log_path = os.path.join(base, "logs_0000000000486000/env000.lz4")
+base = "../rllib/phase2/dense_logs/"
+file_loc = "../rllib/phase2/ckpts/"
+log_path = os.path.join(base, "logs_0000000022728000/env000.lz4")
 dense_log = load_episode_log(log_path)
 (fig0, fig1, fig2), incomes, endows, c_trades, all_builds = plotting.breakdown(dense_log)
 fig = plotting.vis_world_range(dense_log, t0=0, tN=200, N=5)
